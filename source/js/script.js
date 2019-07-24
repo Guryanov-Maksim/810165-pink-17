1a76d94a2e24dd3a523bb8c500621109510a7250
Guryanov-Maksim <guryanov-mv@yandex.ru>
2019-07-24 20:39:10 +0300

    Добавляет скрипты, но они не работоспособны

---
+++ source/js/script.js
@@ -0,0 +1,17 @@
+
+// var pageHeader = document.querySelector('.page-header');
+// var navToggle = document.querySelector('.page-header__toggle');
+
+// pageHeader.classList.remove('page-header--nojs');
+// pageHeader.classList.remove('page-header--opened-menu');
+// pageHeader.classList.add('page-header--closed-menu');
+
+// navToggle.addEventListener('click', function() {
+//   if (navMain.classList.contains('main-nav--closed')) {
+//     navMain.classList.remove('main-nav--closed');
+//     navMain.classList.add('main-nav--opened');
+//   } else {
+//     navMain.classList.add('main-nav--closed');
+//     navMain.classList.remove('main-nav--opened');
+//   }
+// });
